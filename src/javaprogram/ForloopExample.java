3f48d14611fe8d5eb1f01e5f139bff4901605f0a
Monu shah <shahmonu1612@gmail.com>
2020-02-08 20:13:37 +0530

    My First Commit

---
+++ src/javaprogram/ForloopExample.java
@@ -0,0 +1,14 @@
+package javaprogram;
+
+public class ForloopExample {
+
+	public static void main(String[] args) {
+		
+		for(int i=10; i>1; i--) {
+			System.out.println("the value of i is:" + i);
+		}
+
+		
+	}
+
+}
